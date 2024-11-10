f5b3f96035ef35f27f35821910aa198118b970b0
akshaynkotkar <akshaynkotkar@gmail.com>
2024-11-10 09:45:39 +0530

    setting up

---
+++ src/test/java/com/example/introductionto/SampleLearning/SampleLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.introductionto.SampleLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
